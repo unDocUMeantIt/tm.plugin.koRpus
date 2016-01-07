0c23f7c70230b3fee00d88a77b85ba1399d9469b
unDocUMeantIt <m@reaktanz.de>
2016-01-07 15:54:55 +0100

    added "format" to sourcesCorpus() and topicCorpus() as well
      - for internal consistency, topicCorpus() now always expects a list as "paths"

--- R/sourcesCorpus.R
+++ R/sourcesCorpus.R
@@ -17,34 +17,70 @@
 
 #' Function to create kRp.sourcesCorpus objects from directory content
 #' 
-#' @param path A charcter vector with the path to the texts to analyse. Below this path, texts
-#'    must be ordered into subfolders named exactly like defined by \code{sources}.
+#' Can also be used with the texts tot analyze directly.
+#'
+#' @param path Usually a charcter vector with the path to the texts to analyse. Below this path, texts
+#'    must be ordered into subfolders named exactly like defined by \code{sources}. However, if \code{format="obj"},
+#'    you use this argument to provide the texts themselves as a list of named character vectors, where the name
+#'    must match the names of \code{sources} and each element of the vectors is a single text.
 #' @param sources A named character vector defining all sources to regard. The names of each entry will be used
 #'    internally for the sources.
 #' @param topic A character string naming the topic covered.
+#' @param format Either "file" or "obj", depending on whether you want to scan files or analyze the text in a given object, like
+#'    a character vector. See \code{\link[tm.plugin.koRpus:simpleCorpus]{simpleCorpus}} for more information.
 #' @param ... Additional options, passed through to \code{simpleCorpus}.
 #' @import tm
 #' @export
 #' @examples
 #' \dontrun{
 #' myTopic <- sourcesCorpus("~/data/foo", sources=c(bar="The Bar Magazine", baz="BAZ Monthly"))
+#' 
+#' # providing text directly
+#' myTopic <- sourcesCorpus(
+#'   list(
+#'     source1=c("the first text.", "the second text."),
+#'     source2=c("the third text.", "the last text.")
+#'   ),
+#'   sources=c(
+#'     source1="The Important",
+#'     source2="Must Read Magazine"
+#'   ),
+#'   topic="short stories",
+#'   format="obj"
+#' )
 #' }
 
-sourcesCorpus <- function(path, sources, topic="", ...){
+sourcesCorpus <- function(path, sources, topic="", format="file", ...){
 
     all.texts <- new("kRp.sourcesCorpus")
 
-    for (src in names(sources)){
-      slot(all.texts, "paths")[[src]] <- list.files(file.path(path, sources[src]), full.names=TRUE)
-      slot(all.texts, "files")[[src]] <- list.files(file.path(path, sources[src]))
-      numTexts <- length(slot(all.texts, "paths")[[src]])
+    if(identical(format, "file")){
+      for (src in names(sources)){
+        slot(all.texts, "paths")[[src]] <- list.files(file.path(path, sources[src]), full.names=TRUE)
+        slot(all.texts, "files")[[src]] <- list.files(file.path(path, sources[src]))
+        numTexts <- length(slot(all.texts, "paths")[[src]])
 
-      topicText <- ifelse(identical(topic, ""), "", paste0("topic \"", topic, "\", "))
-      message(paste0("processing ", topicText, "source \"", sources[src], "\", ", numTexts, " texts..."))
-      
-      slot(all.texts, "summary")[[src]] <- list()
+        topicText <- ifelse(identical(topic, ""), "", paste0("topic \"", topic, "\", "))
+        message(paste0("processing ", topicText, "source \"", sources[src], "\", ", numTexts, " texts..."))
+        
+        slot(all.texts, "summary")[[src]] <- list()
 
-      slot(all.texts, "sources")[[src]] <- simpleCorpus(dir=file.path(path,sources[src]), source=src, topic=topic, ...)
+        slot(all.texts, "sources")[[src]] <- simpleCorpus(dir=file.path(path,sources[src]), source=src, topic=topic, ...)
+      }
+    } else if(identical(format, "obj")){
+      for (src in names(sources)){
+        numTexts <- length(path[[src]])
+        slot(all.texts, "paths")[[src]] <- slot(all.texts, "files")[[src]] <- rep("", numTexts)
+
+        topicText <- ifelse(identical(topic, ""), "", paste0("topic \"", topic, "\", "))
+        message(paste0("processing ", topicText, "source \"", sources[src], "\", ", numTexts, " texts..."))
+        
+        slot(all.texts, "summary")[[src]] <- list()
+
+        slot(all.texts, "sources")[[src]] <- simpleCorpus(dir=path[[src]], source=src, topic=topic, format=format, ...)
+      }
+    } else {
+      stop(simpleError(paste0("invalid value for \"format\":\n  \"", format, "\"")))
     }
 
     return(all.texts)
