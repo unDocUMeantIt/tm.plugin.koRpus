8e9aa8284566c512730a51179c984b317ceec1dd
unDocUMeantIt <m@reaktanz.de>
2019-08-19 18:41:29 +0200

    new class kRp.flatHier to later replace the recursive objects with a "flat" data frame
    this might need some alterations to the original kRp.tagged class, allowing more columns

---
+++ R/01_class_02_kRp.flatHier.R
@@ -0,0 +1,95 @@
+# Copyright 2019 Meik Michalke <meik.michalke@hhu.de>
+#
+# This file is part of the R package tm.plugin.koRpus.
+#
+# tm.plugin.koRpus is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# tm.plugin.koRpus is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with tm.plugin.koRpus.  If not, see <http://www.gnu.org/licenses/>.
+
+
+#' S4 Class kRp.flatHier
+#'
+#' Objects of this class can contain full text corpora in a hierachical structure. It supports both the \code{tm} package's
+#' \code{\link[tm]{Corpus}} class and \code{koRpus}' own object classes and stores them in separated slots.
+#' 
+#' Objects should be created using the \code{\link[tm.plugin.koRpus:readCorpus]{readCorpus}} function.
+#' 
+#' @section Contructor function:
+#' Should you need to manually generate objects of this class (which should rarely be the case), the contructor function 
+#' \code{kRp_flatHier(...)} can be used instead of
+#' \code{new("kRp.flatHier", ...)}. Whenever possible, stick to
+#' \code{\link[tm.plugin.koRpus:readCorpus]{readCorpus}}.
+#' 
+#' @param hierarchy A named list of named character vectors describing the directory hierarchy level by level.
+#'    If \code{TRUE} instead, the hierarchy structure is taken directly from the directory tree.
+#'    See section Hierarchy for details. 
+#' @slot meta A named list. Can be used to store meta information. Currently, no particular format is defined.
+#' @slot raw A list of objects of class \code{\link[tm]{Corpus}}.
+#' @slot tagged An object of class \code{kRp.taggedText}.
+#' @note There is also \code{\link[tm.plugin.koRpus:kRp.flatHier_get-methods]{getter and setter methods}} for objects of this class.
+#' @name kRp.flatHier,-class
+#' @aliases kRp.flatHier,-class kRp.flatHier-class
+#' @import methods koRpus
+#' @keywords classes
+#' @export kRp_flatHier
+#' @exportClass kRp.flatHier
+#' @rdname kRp.flatHier
+#' @examples
+#' \dontrun{
+#' # use readCorpus() to create objects of class kRp.flatHier
+#' myCorpus <- readCorpus(
+#'   dir=file.path(path.package("tm.plugin.koRpus"), "tests", "testthat", "samples"),
+#'   hierarchy=list(
+#'     Topic=c(
+#'       C3S="C3S",
+#'       GEMA="GEMA"
+#'     ),
+#'     Source=c(
+#'       Wikipedia_alt="Wikipedia (alt)",
+#'       Wikipedia_neu="Wikipedia (neu)"
+#'     )
+#'   )
+#' )
+#' }
+#' # manual creation
+#' emptyCorpus <- kRp_hierarchy()
+kRp_flatHier <- setClass("kRp.flatHier",
+  representation=representation(
+    hierarchy="list",
+    meta="list",
+    raw="list",
+    tagged="kRp.taggedText"
+  ),
+  prototype=prototype(
+    hierarchy=list=(),
+    meta=list(),
+    raw=list(),
+    tagged=list()
+  )
+)
+
+setValidity("kRp.flatHier", function(object){
+  # TODO: check if hierarchy is resembled by rows in tagged object
+    hierarchy <- slot(object, "hierarchy")
+    raw <- slot(object, "raw")
+
+    classObj <- list(
+      "Corpus"=list(name="raw", obj=raw)
+    )
+    for (thisClassObj in names(classObj)) {
+      if(!identical(classObj[[thisClassObj]][["obj"]], list()) && !all(sapply(classObj[[thisClassObj]][["obj"]], function(x) inherits(x, thisClassObj)))){
+        stop(simpleError(paste0("Invalid object: Slot \"", classObj[[thisClassObj]][["name"]], "\" must have entries inheriting from class ", thisClassObj, "!")))
+      } else {}
+    }
+
+    return(TRUE)
+})
